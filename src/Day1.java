bbaa3fedcddd996bbe01b6141bb7f858be3da12d
Gonzalez Martinez, Juan <jgonzalezmartinez@netsuite.com>
2021-12-01 15:56:43 +0100

    Fix in first and last occurrence

--- src/Day1.java
+++ src/Day1.java
@@ -8,18 +8,18 @@ public class Day1 {
 
     public static void main(String[] args) throws Exception{
 
-        List<Integer> measurements = getListOfElements("resources/day1_input.txt");
+        List<Integer> measurements = getListOfElements("resources/test.txt");
 
         System.out.println("Challenge 1: " + getIncreases(measurements, 1));
         System.out.println("Challenge 2: " + getIncreases(measurements, 3));
     }
 
     public static int getIncreases (List<Integer> measurements, int window){
-        int result = 0, previousValue = 0, value;
+        int result = 0, previousValue=0, value;
 
-        for (int i = 0; i < measurements.size()-window; i++){
-            value = measurements.subList(i, i+window).stream().mapToInt(Integer::intValue).sum();
-            if (value>previousValue)
+        for (int i = 0; i < measurements.size()-window+1; i++){
+            value = measurements.subList(i, i + window).stream().mapToInt(Integer::intValue).sum();
+            if (value > previousValue && i!=0) //First occurrence must not be evaluated
                 result++;
             previousValue = value;
         }
